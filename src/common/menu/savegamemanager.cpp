f6dfa6ad6271859a3f296b19730be2565bb5edaa
SanyaWaffles <sanya.waffles@gmail.com>
2021-04-24 15:43:38 -0400

    fix savegame issue

--- src/common/menu/savegamemanager.cpp
+++ src/common/menu/savegamemanager.cpp
@@ -232,7 +232,7 @@ DEFINE_ACTION_FUNCTION(FSavegameManager, LoadSavegame)
 
 void FSavegameManagerBase::DoSave(int Selected, const char *savegamestring)
 {
-	RemoveNewSaveNode();
+	// RemoveNewSaveNode();
 	if (Selected != 0)
 	{
 		auto node = SaveGames[Selected];
