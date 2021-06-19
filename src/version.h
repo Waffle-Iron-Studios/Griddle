cbdec7fa140776501ee20d914a82ed45a3779c8c
SanyaWaffles <sanya.waffles@gmail.com>
2021-06-19 02:42:28 -0400

    fix version.h

--- src/version.h
+++ src/version.h
@@ -41,11 +41,11 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "4.6"
+#define VERSIONSTR "4.7"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 4,6,0
-#define RC_PRODUCTVERSION 4,6,0
+#define RC_FILEVERSION 4,7,9999
+#define RC_PRODUCTVERSION 4,7,9999
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
