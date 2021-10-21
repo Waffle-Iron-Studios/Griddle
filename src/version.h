902e0fd60e08ff419526764ae6fe64aff82feb7a
SanyaWaffles <SanyaWaffles@users.noreply.github.com>
2021-10-21 05:22:03 -0400

    This is 4.7.1

--- src/version.h
+++ src/version.h
@@ -41,21 +41,21 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "4.8pre"
+#define VERSIONSTR "4.7.1"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 4,7,9999,0
-#define RC_PRODUCTVERSION 4,7,9999,0
+#define RC_FILEVERSION 4,7,1
+#define RC_PRODUCTVERSION 4,7,1
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
-#define VER_MINOR 8
-#define VER_REVISION 0
+#define VER_MINOR 7
+#define VER_REVISION 1
 
 // This should always refer to the GZDoom version a derived port is based on and not reflect the derived port's version number!
 #define ENG_MAJOR 4
-#define ENG_MINOR 8
-#define ENG_REVISION 0
+#define ENG_MINOR 7
+#define ENG_REVISION 1
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you
@@ -101,8 +101,8 @@ const char *GetVersionString();
 #define GAMENAME "WIGZDoom"
 #define WGAMENAME L"WIGZDoom"
 #define GAMENAMELOWERCASE "wigzdoom"
-#define FORUM_URL "http://forum.zdoom.org/"
-#define BUGS_FORUM_URL	"http://forum.zdoom.org/viewforum.php?f=2"
+#define FORUM_URL "http://www.waffleironstudios.com/"
+#define BUGS_FORUM_URL	"http://www.waffleironstudios.com/"
 
 #if defined(__APPLE__) || defined(_WIN32)
 #define GAME_DIR GAMENAME
