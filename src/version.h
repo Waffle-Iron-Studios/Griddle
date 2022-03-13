209a0734192190fce6fac4459087b2860c9c0014
SanyaWaffles <SanyaWaffles@users.noreply.github.com>
2022-03-13 19:47:02 -0400

    Revert "Use a generic WIGZDoom RPC as a failsafe"
    This reverts commit 88dc9da672f9909cca4a829c3b92bd76fe56e0c5.

--- src/version.h
+++ src/version.h
@@ -110,7 +110,7 @@ const char *GetVersionString();
 #define GAME_DIR ".config/" GAMENAMELOWERCASE
 #endif
 
-#define DEFAULT_DISCORD_APP_ID "952713776631803934"
+#define DEFAULT_DISCORD_APP_ID "951303644597325885"
 
 const int SAVEPICWIDTH = 216;
 const int SAVEPICHEIGHT = 162;
