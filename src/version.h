d25819f2fec8b7cf2aba67c248986446b22a61fb
SanyaWaffles <SanyaWaffles@users.noreply.github.com>
2022-06-07 21:16:47 -0400

    adjust version

--- src/version.h
+++ src/version.h
@@ -41,11 +41,11 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "4.7.1-rpc"
+#define VERSIONSTR "4.8.0a"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 4,7,1,0
-#define RC_PRODUCTVERSION 4,7,1,0
+#define RC_FILEVERSION 4,8,0,0
+#define RC_PRODUCTVERSION 4,8,0,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
