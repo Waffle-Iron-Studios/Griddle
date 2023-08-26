87272cf0091aac33df6fc897334b509cea5e5b30
SanyaWaffles <SanyaWaffles@users.noreply.github.com>
2023-08-26 15:33:32 -0400

    Clean up some unneeded stuff. Add support for vcpkg

--- src/p_setup.cpp
+++ src/p_setup.cpp
@@ -435,10 +435,7 @@ void P_SetupLevel(FLevelLocals *Level, int position, bool newGame)
 	if (p) p->viewz = NO_VALUE;
 
 	// Make sure all sounds are stopped before Z_FreeTags.
-
-	if (gamestate != GS_TITLELEVEL)
-		S_Start();
-
+	S_Start();
 	S_ResetMusic();
 
 	// Don't start the music if loading a savegame, because the music is stored there.
