f8c51c7c1cac3569881c5092d5fcf219c1abe648
SanyaWaffles <SanyaWaffles@users.noreply.github.com>
2024-01-21 00:12:19 -0500

    Update Settings Panel

--- src/launcher/settingspage.cpp
+++ src/launcher/settingspage.cpp
@@ -24,15 +24,11 @@ SettingsPage::SettingsPage(LauncherWindow* launcher, int* autoloadflags) : Widge
 	FullscreenCheckbox = new CheckboxLabel(this);
 	DisableAutoloadCheckbox = new CheckboxLabel(this);
 	DontAskAgainCheckbox = new CheckboxLabel(this);
-	LightsCheckbox = new CheckboxLabel(this);
-	BrightmapsCheckbox = new CheckboxLabel(this);
-	WidescreenCheckbox = new CheckboxLabel(this);
 
 #ifdef RENDER_BACKENDS
 	BackendLabel = new TextLabel(this);
 	VulkanCheckbox = new CheckboxLabel(this);
 	OpenGLCheckbox = new CheckboxLabel(this);
-	GLESCheckbox = new CheckboxLabel(this);
 #endif
 
 	FullscreenCheckbox->SetChecked(vid_fullscreen);
@@ -40,17 +36,12 @@ SettingsPage::SettingsPage(LauncherWindow* launcher, int* autoloadflags) : Widge
 
 	int flags = *autoloadflags;
 	DisableAutoloadCheckbox->SetChecked(flags & 1);
-	LightsCheckbox->SetChecked(flags & 2);
-	BrightmapsCheckbox->SetChecked(flags & 4);
-	WidescreenCheckbox->SetChecked(flags & 8);
 
 #ifdef RENDER_BACKENDS
 	OpenGLCheckbox->SetRadioStyle(true);
 	VulkanCheckbox->SetRadioStyle(true);
-	GLESCheckbox->SetRadioStyle(true);
-	OpenGLCheckbox->FuncChanged = [this](bool on) { if (on) { VulkanCheckbox->SetChecked(false); GLESCheckbox->SetChecked(false); }};
-	VulkanCheckbox->FuncChanged = [this](bool on) { if (on) { OpenGLCheckbox->SetChecked(false); GLESCheckbox->SetChecked(false); }};
-	GLESCheckbox->FuncChanged = [this](bool on) { if (on) { VulkanCheckbox->SetChecked(false); OpenGLCheckbox->SetChecked(false); }};
+	OpenGLCheckbox->FuncChanged = [this](bool on) { if (on) { VulkanCheckbox->SetChecked(false); }};
+	VulkanCheckbox->FuncChanged = [this](bool on) { if (on) { OpenGLCheckbox->SetChecked(false); }};
 	switch (vid_preferbackend)
 	{
 	case 0:
@@ -58,9 +49,6 @@ SettingsPage::SettingsPage(LauncherWindow* launcher, int* autoloadflags) : Widge
 		break;
 	case 1:
 		VulkanCheckbox->SetChecked(true);
-		break;
-	case 2:
-		GLESCheckbox->SetChecked(true);
 		break;
 	}
 #endif
@@ -114,18 +102,10 @@ void SettingsPage::Save()
 	vid_fullscreen = FullscreenCheckbox->GetChecked();
 	queryiwad = !DontAskAgainCheckbox->GetChecked();
 
-	int flags = 0;
-	if (DisableAutoloadCheckbox->GetChecked()) flags |= 1;
-	if (LightsCheckbox->GetChecked()) flags |= 2;
-	if (BrightmapsCheckbox->GetChecked()) flags |= 4;
-	if (WidescreenCheckbox->GetChecked()) flags |= 8;
-	*AutoloadFlags = flags;
-
 #ifdef RENDER_BACKENDS
 	int v = 1;
 	if (OpenGLCheckbox->GetChecked()) v = 0;
 	else if (VulkanCheckbox->GetChecked()) v = 1;
-	else if (GLESCheckbox->GetChecked()) v = 2;
 	if (v != vid_preferbackend) vid_preferbackend = v;
 #endif
 }
@@ -134,19 +114,14 @@ void SettingsPage::UpdateLanguage()
 {
 	LangLabel->SetText(GStrings("OPTMNU_LANGUAGE"));
 	GeneralLabel->SetText(GStrings("PICKER_GENERAL"));
-	ExtrasLabel->SetText(GStrings("PICKER_EXTRA"));
 	FullscreenCheckbox->SetText(GStrings("PICKER_FULLSCREEN"));
 	DisableAutoloadCheckbox->SetText(GStrings("PICKER_NOAUTOLOAD"));
 	DontAskAgainCheckbox->SetText(GStrings("PICKER_DONTASK"));
-	LightsCheckbox->SetText(GStrings("PICKER_LIGHTS"));
-	BrightmapsCheckbox->SetText(GStrings("PICKER_BRIGHTMAPS"));
-	WidescreenCheckbox->SetText(GStrings("PICKER_WIDESCREEN"));
 
 #ifdef RENDER_BACKENDS
 	BackendLabel->SetText(GStrings("PICKER_PREFERBACKEND"));
 	VulkanCheckbox->SetText(GStrings("OPTVAL_VULKAN"));
 	OpenGLCheckbox->SetText(GStrings("OPTVAL_OPENGL"));
-	GLESCheckbox->SetText(GStrings("OPTVAL_OPENGLES"));
 #endif
 }
 
@@ -167,19 +142,15 @@ void SettingsPage::OnGeometryChanged()
 	double h = GetHeight();
 
 	GeneralLabel->SetFrameGeometry(0.0, y, 190.0, GeneralLabel->GetPreferredHeight());
-	ExtrasLabel->SetFrameGeometry(w - panelWidth, y, panelWidth, ExtrasLabel->GetPreferredHeight());
 	y += GeneralLabel->GetPreferredHeight();
 
 	FullscreenCheckbox->SetFrameGeometry(0.0, y, 190.0, FullscreenCheckbox->GetPreferredHeight());
-	LightsCheckbox->SetFrameGeometry(w - panelWidth, y, panelWidth, LightsCheckbox->GetPreferredHeight());
 	y += FullscreenCheckbox->GetPreferredHeight();
 
 	DisableAutoloadCheckbox->SetFrameGeometry(0.0, y, 190.0, DisableAutoloadCheckbox->GetPreferredHeight());
-	BrightmapsCheckbox->SetFrameGeometry(w - panelWidth, y, panelWidth, BrightmapsCheckbox->GetPreferredHeight());
 	y += DisableAutoloadCheckbox->GetPreferredHeight();
 
 	DontAskAgainCheckbox->SetFrameGeometry(0.0, y, 190.0, DontAskAgainCheckbox->GetPreferredHeight());
-	WidescreenCheckbox->SetFrameGeometry(w - panelWidth, y, panelWidth, WidescreenCheckbox->GetPreferredHeight());
 	y += DontAskAgainCheckbox->GetPreferredHeight();
 
 #ifdef RENDER_BACKENDS
@@ -192,10 +163,7 @@ void SettingsPage::OnGeometryChanged()
 	y += VulkanCheckbox->GetPreferredHeight();
 
 	OpenGLCheckbox->SetFrameGeometry(x, y, 190.0, OpenGLCheckbox->GetPreferredHeight());
-	y += OpenGLCheckbox->GetPreferredHeight();
-
-	GLESCheckbox->SetFrameGeometry(x, y, 190.0, GLESCheckbox->GetPreferredHeight());
-	y += GLESCheckbox->GetPreferredHeight();
+	y += OpenGLCheckbox->GetPreferredHeight() * 2;
 #endif
 
 	if (!hideLanguage)
