38c149d71e48548937cc28872410eb0ccd7aea7b
SanyaWaffles <SanyaWaffles@users.noreply.github.com>
2024-04-22 18:28:28 -0400

    Unblock CVars from being changed in non menus.
    Unblock CVars from being changed in non menus. General consensus with dev team and HFS is this is moot as you can call ThrowAbortException anywhere in ZScript and that can be abused.

--- src/common/scripting/interface/vmnatives.cpp
+++ src/common/scripting/interface/vmnatives.cpp
@@ -886,14 +886,6 @@ DEFINE_ACTION_FUNCTION(_CVar, SetInt)
 {
 	// Only menus are allowed to change CVARs.
 	PARAM_SELF_STRUCT_PROLOGUE(FBaseCVar);
-	if (!(self->GetFlags() & CVAR_MOD))
-	{
-		// Only menus are allowed to change non-mod CVARs.
-		if (DMenu::InMenu == 0)
-		{
-			ThrowAbortException(X_OTHER, "Attempt to change CVAR '%s' outside of menu code", self->GetName());
-		}
-	}
 	PARAM_INT(val);
 	UCVarValue v;
 	v.Int = val;
@@ -919,14 +911,6 @@ DEFINE_ACTION_FUNCTION(_CVar, SetInt)
 DEFINE_ACTION_FUNCTION(_CVar, SetFloat)
 {
 	PARAM_SELF_STRUCT_PROLOGUE(FBaseCVar);
-	if (!(self->GetFlags() & CVAR_MOD))
-	{
-		// Only menus are allowed to change non-mod CVARs.
-		if (DMenu::InMenu == 0)
-		{
-			ThrowAbortException(X_OTHER, "Attempt to change CVAR '%s' outside of menu code", self->GetName());
-		}
-	}
 	PARAM_FLOAT(val);
 	UCVarValue v;
 	v.Float = (float)val;
@@ -953,14 +937,6 @@ DEFINE_ACTION_FUNCTION(_CVar, SetString)
 {
 	// Only menus are allowed to change CVARs.
 	PARAM_SELF_STRUCT_PROLOGUE(FBaseCVar);
-	if (!(self->GetFlags() & CVAR_MOD))
-	{
-		// Only menus are allowed to change non-mod CVARs.
-		if (DMenu::InMenu == 0)
-		{
-			ThrowAbortException(X_OTHER, "Attempt to change CVAR '%s' outside of menu code", self->GetName());
-		}
-	}
 	PARAM_STRING(val);
 	UCVarValue v;
 	v.String = val.GetChars();
@@ -992,15 +968,6 @@ DEFINE_ACTION_FUNCTION(_CVar, GetRealType)
 DEFINE_ACTION_FUNCTION(_CVar, ResetToDefault)
 {
 	PARAM_SELF_STRUCT_PROLOGUE(FBaseCVar);
-	if (!(self->GetFlags() & CVAR_MOD))
-	{
-		// Only menus are allowed to change non-mod CVARs.
-		if (DMenu::InMenu == 0)
-		{
-			ThrowAbortException(X_OTHER, "Attempt to change CVAR '%s' outside of menu code", self->GetName());
-		}
-	}
-
 	self->ResetToDefault();
 	return 0;
 }
