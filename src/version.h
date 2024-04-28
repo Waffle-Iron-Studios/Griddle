825660dd49afbcffb0ebbfaef520707ae0e10889
SanyaWaffles <SanyaWaffles@users.noreply.github.com>
2024-04-28 16:54:04 -0400

    Merge branch 'g4.12' of https://github.com/coelckers/gzdoom

--- src/version.h
+++ src/version.h
@@ -41,26 +41,26 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "1.6.2 (RF Version)"
+#define VERSIONSTR "1.6.3"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 1,6,2,0
-#define RC_PRODUCTVERSION 1,6,2,0
+#define RC_FILEVERSION 1,6,3,0
+#define RC_PRODUCTVERSION 1,6,3,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 1
 #define VER_MINOR 6
-#define VER_REVISION 2
+#define VER_REVISION 3
 
 // These are for ZScript versioning
 #define ZSCRIPT_VER_MAJOR 4
 #define ZSCRIPT_VER_MINOR 12
-#define ZSCRIPT_VER_REVISION 1
+#define ZSCRIPT_VER_REVISION 3
 
 // This should always refer to the GZDoom version a derived port is based on and not reflect the derived port's version number!
 #define ENG_MAJOR 4
 #define ENG_MINOR 12
-#define ENG_REVISION 1
+#define ENG_REVISION 3
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you
