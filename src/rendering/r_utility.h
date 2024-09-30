6d6a7857e3be630aed14154889e5e67169cfc288
SanyaWaffles <SanyaWaffles@users.noreply.github.com>
2024-09-29 20:06:22 -0400

    Revert round robin/iso stuff as it was breaking clipping

--- src/rendering/r_utility.h
+++ src/rendering/r_utility.h
@@ -33,16 +33,10 @@ struct FRenderViewpoint
 	double			Sin;			// sin(Angles.Yaw)
 	double			TanCos;			// FocalTangent * cos(Angles.Yaw)
 	double			TanSin;			// FocalTangent * sin(Angles.Yaw)
-	double			PitchCos;		// cos(Angles.Pitch)
-	double			PitchSin;		// sin(Angles.Pitch)
-	double			floordistfact;	// used for isometric sprites Y-billboarding compensation in hw_sprites.cpp
-	double			cotfloor;		// used for isometric sprites Y-billboarding compensation in hw_sprites.cpp
-	angle_t		FrustAngle; 	// FrustumAngle() result
 
 	AActor			*camera;		// camera actor
 	sector_t		*sector;		// [RH] keep track of sector viewing from
 	DAngle			FieldOfView;	// current field of view
-	double			ScreenProj;	// Screen projection factor for orthographic projection
 
 	double			TicFrac;		// fraction of tic for interpolation
 	uint32_t		FrameTime;		// current frame's time in tics.
@@ -51,8 +45,6 @@ struct FRenderViewpoint
 	bool			showviewer;		// show the camera actor?
 	bool			bForceNoViewer; // Never show the camera Actor.
 	void SetViewAngle(const FViewWindow& viewWindow);
-	bool IsAllowedOoB();				// Checks if camera actor exists, has viewpos, and viewpos has VPSF_ALLOWOUTOFBOUNDS flag set
-	bool IsOrtho();					// Checks if camera actor exists, has viewpos, and viewpos has VPSF_ORTHOGRAPHIC flag set
 
 };
 
