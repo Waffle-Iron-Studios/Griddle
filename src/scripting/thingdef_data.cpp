97f185201a079917ee6374bd49b4bc95b901ba06
SanyaWaffles <SanyaWaffles@users.noreply.github.com>
2024-10-25 08:00:03 -0400

    Revert revert iso stuff for easier merging
    we won't use it, but I'm not sifting through thirty thousand merges either

--- src/scripting/thingdef_data.cpp
+++ src/scripting/thingdef_data.cpp
@@ -388,6 +388,7 @@ static FFlagDef ActorFlagDefs[]=
 	DEFINE_FLAG(RF2, FLIPSPRITEOFFSETY, AActor, renderflags2),
 	DEFINE_FLAG(RF2, CAMFOLLOWSPLAYER, AActor, renderflags2),
 	DEFINE_FLAG(RF2, NOMIPMAP, AActor, renderflags2),
+	DEFINE_FLAG(RF2, ISOMETRICSPRITES, AActor, renderflags2),
 	DEFINE_FLAG(RF2, SQUAREPIXELS, AActor, renderflags2),
 
 	// Bounce flags
