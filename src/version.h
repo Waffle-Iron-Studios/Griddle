7ac8882ed9ebeabade7ed496767aeefc311e43f1
SanyaWaffles <SanyaWaffles@users.noreply.github.com>
2025-03-05 18:32:34 -0500

    Update version.h

--- src/version.h
+++ src/version.h
@@ -54,7 +54,7 @@ const char *GetVersionString();
 
 // These are for ZScript versioning
 #define ZSCRIPT_VER_MAJOR 4
-#define ZSCRIPT_VER_MINOR 14
+#define ZSCRIPT_VER_MINOR 15
 #define ZSCRIPT_VER_REVISION 0
 
 // This should always refer to the GZDoom version a derived port is based on and not reflect the derived port's version number!
