825b4fddf31c027919dc67f3bfafd44e51b44085
SanyaWaffles <SanyaWaffles@users.noreply.github.com>
2026-01-08 19:03:33 -0500

    Hellslinger branding

--- src/version.h
+++ src/version.h
@@ -83,7 +83,7 @@ const char *GetVersionString();
 // SAVESIG should match SAVEVER.
 
 // extension for savegames
-#define SAVEGAME_EXT "wds"
+#define SAVEGAME_EXT "hssv"
 
 // MINSAVEVER is the minimum level snapshot version that can be loaded.
 #define MINSAVEVER 4556
@@ -93,7 +93,7 @@ const char *GetVersionString();
 #define SAVEVER 4560
 
 // This is so that derivates can use the same savegame versions without worrying about engine compatibility
-#define GAMESIG "GRIDDLE"
+#define GAMESIG "HELLSLINGER"
 #define BASEWAD "griddle.pk3"
 // Set OPTIONALWAD to "" (null) to disable searching for it
 #define OPTIONALWAD ""
@@ -101,11 +101,11 @@ const char *GetVersionString();
 #define VR3D_ENABLED
 
 // More stuff that needs to be different for derivatives.
-#define GAMENAME "Griddle"
-#define WGAMENAME L"Griddle"
-#define GAMENAMELOWERCASE "griddle"
-#define FORUM_URL "http://www.waffleironstudios.com"
-#define BUGS_FORUM_URL	"http://www.waffleironstudios.com"
+#define GAMENAME "Hellslinger"
+#define WGAMENAME L"Hellslinger"
+#define GAMENAMELOWERCASE "hellslinger"
+#define FORUM_URL ""
+#define BUGS_FORUM_URL	""
 
 #if defined(__APPLE__) || defined(_WIN32)
 #define GAME_DIR GAMENAME
